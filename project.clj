e7bb574a049e6923988df2d5a81426f5dee6f936
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2015-08-26 09:23:00 +0200

    Changed back to main and uberjar deployment.

--- project.clj
+++ project.clj
@@ -11,8 +11,11 @@
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.2"]
                  [hiccup "1.0.5"]]
+  :main ^:skip-aot shouter.web
+  :uberjar-name "shouter-standalone.jar"
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler shouter.web/application
          :init shouter.models.migration/migrate}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                                  [ring-mock "0.1.5"]]}})
+                                  [ring-mock "0.1.5"]]}
+             :uberjar {:aot :all}})
