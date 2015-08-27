11daaaecb56970c593e8748c2e203bf4383e8fe2
Joe Kutner <jpkutner@gmail.com>
2015-08-26 19:43:53 -0500

    removed lein-ring

--- project.clj
+++ project.clj
@@ -6,14 +6,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.4.1"]
-                 [postgresql "9.3-1102.jdbc41"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.2"]
                  [hiccup "1.0.5"]]
   :main ^:skip-aot shouter.web
   :uberjar-name "shouter-standalone.jar"
-  :plugins [[lein-ring "0.8.13"]]
   :ring {:handler shouter.web/application
          :init shouter.models.migration/migrate}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
